88f6e9d467afc8a61bfc10a2293056e751a19351
Y0ungOne <116868164+Y0ungOne@users.noreply.github.com>
2022-11-24 03:08:37 +0900

    22.11.24

--- src/gui/MainPage.java
+++ src/gui/MainPage.java
@@ -14,6 +14,7 @@ public class MainPage extends JFrame {
     public MainPage(Store store) {
         JFrame frame = new BasicFormat(store);
         frame.setTitle("메인화면");
+        Font font = new Font("Binggrae-Bold",Font.BOLD, 10);
 
         // Tips패널
         JPanel TipsPanel = new JPanel();
@@ -21,18 +22,22 @@ public class MainPage extends JFrame {
         TipsPanel.setBounds(5, 5, 390, 415);
 
         int randomNumber1;
-        randomNumber1 = (int) (Math.random()*2+1);
-        ImageIcon tipImgIcon = new ImageIcon("./image/Tips"+randomNumber1+".JPEG");
+        randomNumber1 = (int) (Math.random()*6+1);
+        ImageIcon tipImgIcon = new ImageIcon("./image/tip/Tips"+randomNumber1+".jpg");
         JLabel FirstImg = new JLabel(tipImgIcon);
         FirstImg.setBounds(0, 0, 390, 200);
-
+        //중복 제거 완료
         int randomNumber2;
-        randomNumber2 = (int)(Math.random()*2+1);
-        while (randomNumber2 != randomNumber1){
-            randomNumber2 = (int)(Math.random()*2+1);
+        randomNumber2 = (int)(Math.random()*30+1);
+        while (true){
+            if(randomNumber2 == randomNumber1)
+                randomNumber2=(int)(Math.random()*30+1);
+            else
+                break;
+
         }
         System.out.println("2:" + randomNumber2 + "1: " + randomNumber1);
-        ImageIcon tipImgIcon2 = new ImageIcon("./image/Tips"+randomNumber2+".JPEG");
+        ImageIcon tipImgIcon2 = new ImageIcon("./image/tip/Tips"+randomNumber2+".jpg");
         JLabel SecTip = new JLabel(tipImgIcon2);
         SecTip.setBounds(0, 210, 390, 200);
 
@@ -45,7 +50,7 @@ public class MainPage extends JFrame {
         JPanel MainButtonPanel = new JPanel();
         MainButtonPanel.setLayout(null);
         MainButtonPanel.setBounds(5, 420, 390, 90);
-
+        //이미지를 넣어보고 싶었는데 사이즈이슈로 그냥 글씨나오는 패널로 할게요...
         JButton LikedListModify = new JButton("찜 수정");
         LikedListModify.setBounds(0, 5, 190, 80);
 
