96d90f56095ddcdb8eff6f19c0acc4d0d53a02fa
hyeeuncho <heeuncho@naver.com>
2022-11-24 14:48:37 +0900

    2022-11-24조혜은
    결과창,상세페이지 연결

--- src/food/Store.java
+++ src/food/Store.java
@@ -5,10 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
-import gui.DetailPage;
-import gui.LikedList;
-import gui.MainPage;
-import gui.MyFridge;
+import gui.*;
 import mgr.Factory;
 import mgr.Manager;
 
@@ -101,9 +98,9 @@ public class Store extends Manager {
 
 		// gui의 메인 클래스를 호출하는 것이고, food객체, user객체 입니다.
 		// 박동민의 page 형식에 맞는 호출 값이기 때문에 다른 분들은 다른 형식의 호출을 진행하셔도 됩니다.
-		//new MainPage(this);
+		new MainPage(this);
 		//new SearchResult(this);
-		new LikedList(this);
+		//new LikedList(this);
 		//new MyFridge(this);
 		//new DetailPage(this,foodMgr.getList().get(0));
 
